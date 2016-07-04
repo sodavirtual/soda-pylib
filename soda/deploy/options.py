792cb216a3c838929fd3e2f94fbb931ff45a3022
Evandro Myller <eMyller@7ws.co>
2016-07-04 19:08:24 -0300

    Ask the user if they really want to force things

--- soda/deploy/options.py
+++ soda/deploy/options.py
@@ -1,8 +1,14 @@
-from fabric.api import env, task
+from fabric.api import abort, env, task
+from fabric.colors import red
+
+from soda.deploy import misc
 
 
 @task
 def force():
     """Mark the `force` flag
     """
+    msg = 'You are forcing things. Are you aware of possible damages?'
+    if misc.input(red('{} (y/n) '.format(msg))) != 'y':
+        abort('Wise.')
     env.force = True
