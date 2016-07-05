699c0d4769a6924c74b2db01523ba8a7b3ea5cc0
Evandro Myller <eMyller@7ws.co>
2016-07-05 19:04:44 -0300

    Rewrite the lock approach into a decorator

--- soda/deploy/lock.py
+++ soda/deploy/lock.py
@@ -1,28 +1,39 @@
-from fabric.api import run, task, local
+import os.path
+
+from fabric.api import cd, env, hide, run
 from fabric.context_managers import settings
+from fabric.contrib import files
 
 from soda.deploy import misc
 
 
-@task
-def verify_lock_file():
-    role, roledef = misc.get_effective_role()
-    logged_user = settings(user=roledef['user'])
-    with logged_user:
-        return run('[ -e deploy.lock ] && echo 1 || echo 0 ')
+class lock_task(object):
 
+    """A decorator that creates a `lock` file for a Fabric task
+    """
 
-@task
-def create_lock_file():
-    role, roledef = misc.get_effective_role()
-    logged_user = settings(user=roledef['user'])
-    with logged_user:
-        local('touch deploy.lock')
+    def __init__(self, fn):
+        self.fn = fn
 
+    def __call__(self, *args, **kwargs):
+        role, roledef = misc.get_effective_role()
 
-@task
-def delete_lock_file():
-    role, roledef = misc.get_effective_role()
-    logged_user = settings(user=roledef['user'])
-    with logged_user:
-        local('rm deploy.lock')
+        with settings(user=roledef['user']):
+            lock_file = os.path.join(roledef['app_path'], 'soda-lock')
+            file_exists = files.exists(lock_file)
+
+        if file_exists:
+            msg = 'There is a conflicting operation ongoing or broken.'
+            misc.error(msg, abort_task=not env.force)
+
+        logged_user = settings(user=roledef['user'])
+        cwd = cd(roledef['app_path'])
+        with hide('everything'), logged_user, cwd:
+            run('touch soda-lock')
+
+        self.fn(*args, **kwargs)
+
+        logged_user = settings(user=roledef['user'])
+        cwd = cd(roledef['app_path'])
+        with hide('everything'), logged_user, cwd:
+            run('rm soda-lock')
