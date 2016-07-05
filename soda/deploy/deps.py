edd670a84dd6ee104a8662631551d0c8ff2fea50
Ricardo Meireles <cadomeireles@gmail.com>
2016-07-05 10:27:13 -0300

    Methods to handle lock deploy file

--- soda/deploy/deps.py
+++ soda/deploy/deps.py
@@ -1,6 +1,6 @@
 import os
 
-from fabric.api import run, task
+from fabric.api import run, task, local
 from fabric.context_managers import cd, prefix, settings
 
 from soda.deploy import misc
@@ -32,3 +32,27 @@ def install_bower_libs():
     misc.info('Updating front-end dependencies...')
     with logged_user, cwd:
         run('bower install')
+
+
+@task
+def verify_lock_file():
+    role, roledef = misc.get_effective_role()
+    logged_user = settings(user=roledef['user'])
+    with logged_user:
+        return run('[ -e deploy.lock ] && echo 1 || echo 0 ')
+
+
+@task
+def create_lock_file():
+    role, roledef = misc.get_effective_role()
+    logged_user = settings(user=roledef['user'])
+    with logged_user:
+        local('touch deploy.lock')
+
+
+@task
+def delete_lock_file():
+    role, roledef = misc.get_effective_role()
+    logged_user = settings(user=roledef['user'])
+    with logged_user:
+        local('rm deploy.lock')
