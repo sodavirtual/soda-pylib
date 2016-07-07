065e5f8a81d3271b9aedca1515ca4aba23617301
Ricardo Meireles <cadomeireles@gmail.com>
2016-07-07 10:44:32 -0300

    Migrate ssl tasks to new-style tasks

--- soda/host/ssl.py
+++ soda/host/ssl.py
@@ -1,73 +1,91 @@
 from __future__ import absolute_import
 
-from fabric.api import execute, run, task
+from fabric.api import execute, run
 from fabric.context_managers import cd, settings
+from fabric.tasks import Task
 
 from soda.misc import display, get_effective_role, input
 
 
-@task
-def create_cert():
+__all__ = [
+    'create_cert',
+    'renew_certs',
+]
+
+
+class CreateCert(Task):
     """Create a standalone SSL certificate for the domains chain
     """
-    from soda.host import nginx  # Import here to avoid wrong Fabric --list
 
-    # Stop nginx first
-    execute(nginx.stop)
+    name = 'create_cert'
 
-    # Retrieve necessary information from the user
-    email = input('Insert the certificate manager email address: ')
-    domains = input('Insert the domains to apply: ').split()
+    def run(self):
+        from soda.host import nginx  # Import here to avoid wrong Fabric --list
 
-    role, roledef = get_effective_role()
-    logged_user = settings(user='root')
-    cwd = cd(roledef.get('letsencrypt_dir', '/opt/letsencrypt'))
-    warn_only = settings(warn_only=True)
+        # Stop nginx first
+        execute(nginx.stop)
 
-    # Generate the certificate
-    with logged_user, cwd, warn_only:
-        result = run((
-            './letsencrypt-auto certonly --standalone '
-            '--email {email} {domains}'
-            .format(
-                email=email,
-                domains=' '.join('-d {}'.format(d) for d in domains),
-            )
-        ))
+        # Retrieve necessary information from the user
+        email = input('Insert the certificate manager email address: ')
+        domains = input('Insert the domains to apply: ').split()
 
-    # Display a result message
-    if result.succeeded:
-        display.success('Key chain successfully created!')
-    else:
-        display.error('Failed to create key chain.', abort_task=False)
+        role, roledef = get_effective_role()
+        logged_user = settings(user='root')
+        cwd = cd(roledef.get('letsencrypt_dir', '/opt/letsencrypt'))
+        warn_only = settings(warn_only=True)
 
-    # Put nginx back up
-    execute(nginx.start)
+        # Generate the certificate
+        with logged_user, cwd, warn_only:
+            result = run((
+                './letsencrypt-auto certonly --standalone '
+                '--email {email} {domains}'
+                .format(
+                    email=email,
+                    domains=' '.join('-d {}'.format(d) for d in domains),
+                )
+            ))
+
+        # Display a result message
+        if result.succeeded:
+            display.success('Key chain successfully created!')
+        else:
+            display.error('Failed to create key chain.', abort_task=False)
+
+        # Put nginx back up
+        execute(nginx.start)
 
 
-@task
-def renew_certs():
+class RenewCerts(Task):
     """Renew SSL certificates
     """
-    from soda.host import nginx  # Import here to avoid wrong Fabric --list
 
-    # Stop nginx first
-    execute(nginx.stop)
+    name = 'renew_certs'
 
-    role, roledef = get_effective_role()
-    logged_user = settings(user='root')
-    cwd = cd(roledef.get('letsencrypt_dir', '/opt/letsencrypt'))
-    warn_only = settings(warn_only=True)
+    def run(self):
+        from soda.host import nginx  # Import here to avoid wrong Fabric --list
 
-    # Generate the certificate
-    with logged_user, cwd, warn_only:
-        result = run('./letsencrypt-auto renew --standalone')
+        # Stop nginx first
+        execute(nginx.stop)
 
-    # Display a result message
-    if result.succeeded:
-        display.success('SSL certificates were successfully renewed!')
-    else:
-        display.error('Failed to renew SSL certificates.', abort_task=False)
+        role, roledef = get_effective_role()
+        logged_user = settings(user='root')
+        cwd = cd(roledef.get('letsencrypt_dir', '/opt/letsencrypt'))
+        warn_only = settings(warn_only=True)
 
-    # Put nginx back up
-    execute(nginx.start)
+        # Generate the certificate
+        with logged_user, cwd, warn_only:
+            result = run('./letsencrypt-auto renew --standalone')
+
+        # Display a result message
+        if result.succeeded:
+            display.success('SSL certificates were successfully renewed!')
+        else:
+            display.error(
+                'Failed to renew SSL certificates.', abort_task=False)
+
+        # Put nginx back up
+        execute(nginx.start)
+
+
+create_cert = CreateCert()
+renew_certs = RenewCerts()
