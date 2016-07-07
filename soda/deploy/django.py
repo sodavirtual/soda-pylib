89a2f269f432f5279d8c29efb3c0934e84a9fed2
Ricardo Meireles <cadomeireles@gmail.com>
2016-07-07 10:37:08 -0300

    Migrate django tasks to new-style tasks

--- soda/deploy/django.py
+++ soda/deploy/django.py
@@ -1,48 +1,65 @@
 from __future__ import absolute_import
 import os
 
-from fabric.api import run, task
+from fabric.api import run
 from fabric.context_managers import cd, prefix, settings, shell_env
+from fabric.tasks import Task
 
 from soda.misc import display, get_effective_role, lock_task
 
 
-@task
-@lock_task
-def collectstatic():
+__all__ = [
+    'collect_static',
+    'migrate',
+]
+
+
+class CollectStaticTask(Task):
     """Run Django's `collectstatic` management command
     """
-    role, roledef = get_effective_role()
-    logged_user = settings(user=roledef['user'])
-    cwd = cd(roledef['app_path'])
-    active_venv = prefix('source {}'.format(
-        os.path.join(roledef['venv_path'], 'bin', 'activate')))
-    django_settings = shell_env(
-        DJANGO_SETTINGS_MODULE=roledef['settings_module'])
 
-    display.info('Collecting static files...')
-    with logged_user, cwd, active_venv, django_settings:
-        ignore = ' '.join(
-            "--ignore '{}'".format(pattern) for pattern in [
-                'cdn', 'debug_toolbar', 'django_extensions', 'less',
-                'package.json', 'README*', 'scss',
-            ])
-        run('./manage.py collectstatic --noinput {}'.format(ignore))
+    name = 'collect_static'
+
+    @lock_task
+    def run(self):
+        role, roledef = get_effective_role()
+        logged_user = settings(user=roledef['user'])
+        cwd = cd(roledef['app_path'])
+        active_venv = prefix('source {}'.format(
+            os.path.join(roledef['venv_path'], 'bin', 'activate')))
+        django_settings = shell_env(
+            DJANGO_SETTINGS_MODULE=roledef['settings_module'])
+
+        display.info('Collecting static files...')
+        with logged_user, cwd, active_venv, django_settings:
+            ignore = ' '.join(
+                "--ignore '{}'".format(pattern) for pattern in [
+                    'cdn', 'debug_toolbar', 'django_extensions', 'less',
+                    'package.json', 'README*', 'scss',
+                ])
+            run('./manage.py collectstatic --noinput {}'.format(ignore))
 
 
-@task
-@lock_task
-def migrate():
+class MigrateTask(Task):
     """Run Django's `migrate` management command
     """
-    role, roledef = get_effective_role()
-    logged_user = settings(user=roledef['user'])
-    cwd = cd(roledef['app_path'])
-    active_venv = prefix('source {}'.format(
-        os.path.join(roledef['venv_path'], 'bin', 'activate')))
-    django_settings = shell_env(
-        DJANGO_SETTINGS_MODULE=roledef['settings_module'])
 
-    display.info('Running database migrations...')
-    with logged_user, cwd, active_venv, django_settings:
-        run('./manage.py migrate --noinput')
+    name = 'migrate'
+
+    @lock_task
+    def run(self):
+        role, roledef = get_effective_role()
+        logged_user = settings(user=roledef['user'])
+        cwd = cd(roledef['app_path'])
+        active_venv = prefix('source {}'.format(
+            os.path.join(roledef['venv_path'], 'bin', 'activate')))
+        django_settings = shell_env(
+            DJANGO_SETTINGS_MODULE=roledef['settings_module'])
+
+        display.info('Running database migrations...')
+        with logged_user, cwd, active_venv, django_settings:
+            run('./manage.py migrate --noinput')
+
+
+collect_static = CollectStaticTask()
+migrate = MigrateTask()
