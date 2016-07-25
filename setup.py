2475e44ff1119675511c0d4438d5d680a7c1804e
Evandro Myller <eMyller@7ws.co>
2016-07-25 01:35:28 -0300

    Replace standard Fabric by Fabric3 to support Python 3

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'fabric',
+        'Fabric3',
     ],
 
     # List additional groups of dependencies here (e.g. development
